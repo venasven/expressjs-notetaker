9009422b2a9a8ca1289ed0da220479c614a7dbfc
venasven <honhonlem@gmail.com>
2023-11-07 02:04:46 -0800

    minor errors fixed

--- routes/notes.js
+++ routes/notes.js
@@ -1,35 +1,41 @@
 const notes = require('express').Router();
-const { readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
 
 notes.get('/', (req, res) =>
-  readFromFile('./db.json').then((data) => res.json(JSON.parse(data)))
+  readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to retrieve notes' });
+    })
 );
 
-
 notes.post('/', (req, res) => {
- 
   const { title, text } = req.body;
 
-
   if (title && text) {
-  
     const newNote = {
       title,
       text,
+      id: uuid(),
     };
 
-    readAndAppend(newNote, './db');
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    res.json(response);
+    readAndAppend(newNote, './db/db.json')
+      .then(() => {
+        const response = {
+          status: 'success',
+          body: newNote,
+        };
+        res.json(response);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to save note' });
+      });
   } else {
-    res.json('Error in posting ');
+    res.status(400).json({ error: 'Invalid request' });
   }
 });
-
 module.exports = notes;
--- server.js
+++ server.js
@@ -10,14 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-// GET Route for feedback page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
